2b5aeb7cf2a3b5f0505c11158ce9f64ba1804f25
Tomasz Gigiel <tomasz.gigiel@gmail.com>
2019-12-28 11:46:24 +0100

    macro qam

--- src/main/clojure/pl/tomaszgigiel/clojure_everyday/etudes.clj
+++ src/main/clojure/pl/tomaszgigiel/clojure_everyday/etudes.clj
@@ -1,3 +1,13 @@
 (ns pl.tomaszgigiel.clojure-everyday.etudes
+  (:require [clojure.test :as tst])
   (:require [pl.tomaszgigiel.utils.misc :as misc])
   (:gen-class))
+
+(defmacro q "question" [s] s)
+(defmacro a "answer" [form message] (list 'tst/is form message))
+(defmacro m "metadata" [s] s)
+
+(defmacro qam
+  "question answer metadata"
+  [q & body]
+  (list 'tst/deftest (symbol (str q)) (cons 'tst/testing (cons q body))))
