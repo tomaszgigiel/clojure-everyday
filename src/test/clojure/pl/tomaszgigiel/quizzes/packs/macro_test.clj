13699330875c3914f5acbb0e65dccd51904b5708
Tomasz Gigiel <tomasz.gigiel@gmail.com>
2020-01-31 17:47:44 +0100

    macro

--- src/test/clojure/pl/tomaszgigiel/quizzes/packs/macro_test.clj
+++ src/test/clojure/pl/tomaszgigiel/quizzes/packs/macro_test.clj
@@ -53,39 +53,82 @@
   (a "inside the body of defmacro you can call it to get an information")
   (m "https://dzone.com/articles/clojure-env-and-form"))
 
+(defmacro show-env-keyword [] (mapv (fn [x] [(keyword x) x]) (keys &env)))
+(defmacro show-env-string [] (mapv (fn [x] [(.name x) (.sym x)]) (vals &env)))
+
 (qam
   (q "Discuss the macro hidden argument &env.")
-  (a (defmacro show-env [] (mapv (fn [x] [(keyword x) x]) (keys &env))))
-  (a (= (eval "(let [foo 1 boo 2] (show-env))") [[:foo 1] [:boo 1]]))
-  (a (defmacro show-env [] (mapv (fn [x] [(.name x) (.sym x)]) (vals &env))))
-  (a (= (eval "(let [foo 1 boo 2] (show-env))") [["foo" 1] ["boo" 2]]))
-  (a false "why exception without eval?")
+  (a "(defmacro show-env-keyword [] (mapv (fn [x] [(keyword x) x]) (keys &env)))")
+  (a (= (show-env-keyword) []))
+  (a (= (let [foo 1 boo 2] (show-env-keyword)) [[:foo 1] [:boo 2]]))
+  (a "(defmacro show-env-string [] (mapv (fn [x] [(.name x) (.sym x)]) (vals &env)))")
+  (a (= (let [foo 1 boo 2] (show-env-string)) [["foo" 1] ["boo" 2]]))
   (m "https://dzone.com/articles/clojure-env-and-form")
   (m "https://www.javadoc.io/doc/org.clojure/clojure/latest/clojure/lang/Compiler.LocalBinding.html"))
 
+(defmacro show-env [] (mapv (fn [x] [(class x) (name x) x]) (keys &env)))
+
 (qam
   (q "Discuss the macro hidden argument &env (how to return symbol?).")
-  (a (defmacro show-env [] (mapv (fn [x] [(class x) (name x) (str x) x]) (keys &env))))
-  (a (= (eval "(let [foo 1 boo 2] (show-env))") [[clojure.lang.Symbol "foo" "foo" 1] [clojure.lang.Symbol "boo" "boo" 2]]))
-  (a false "not a symbol")
-  (a false "why exception without eval?")
+  (a "(defmacro show-env [] (mapv (fn [x] [(class x) (name x) x]) (keys &env)))")
+  (a (= (let [foo 1 boo 2] (show-env)) [[clojure.lang.Symbol "foo" 1] [clojure.lang.Symbol "boo" 2]]))
   (m "https://dzone.com/articles/clojure-env-and-form"))
+
+(defmacro show-form [x y z] (reduce conj [(name (first &form))] (rest &form)))
 
 (qam
   (q "Discuss the macro hidden argument &form.")
-  (a false "todo")
+  (a "(defmacro show-form [x y z] (reduce conj [(name (first &form))] (rest &form)))")
+  (a (= (show-form 1 2 3) ["show-form" 1 2 3]))
   (m "https://dzone.com/articles/clojure-env-and-form"))
 
+(defmacro multi-arity-macro
+  ([] `(multi-arity-macro 0))
+  ([a] `(multi-arity-macro ~a 1))
+  ([a b] `(multi-arity-macro ~a ~b 2))
+  ([a b c] `[~a ~b ~c 3]))
+
 (qam
-  (q "Discuss the multi-arity macro.")
-  (a false "todo")
+  (q "Discuss the multi-arity macro. (macro)")
+  (a "(defmacro multi-arity-macro")
+  (a "([] `(multi-arity-macro 0))")
+  (a "([a] `(multi-arity-macro ~a 1))")
+  (a "([a b] `(multi-arity-macro ~a ~b 2))")
+  (a "([a b c] `[~a ~b ~c 3]))")
+  (a ( = (multi-arity-macro) [0 1 2 3]))
+  (a ( = (multi-arity-macro \a) [\a 1 2 3]))
   (m "https://stackoverflow.com/questions/25566146/multiple-arity-in-defmacro-of-clojure/25569059"))
+
+(defn- multi-arity-function
+  ([] (multi-arity-function 0))
+  ([a] (multi-arity-function a 1))
+  ([a b] (multi-arity-function a b 2))
+  ([a b c] `[ ~a ~b ~c 3]))
+
+(defmacro multi-arity-macro [& args] (apply multi-arity-function args))
+
+(qam
+  (q "Discuss the multi-arity macro. (fun)")
+  (a "(defn- multi-arity-function")
+  (a "([] (multi-arity-function 0))")
+  (a "([a] (multi-arity-function a 1))")
+  (a "([a b] (multi-arity-function a b 2))")
+  (a "([a b c] `[ ~a ~b ~c 3]))")
+  (a "(defmacro multi-arity-macro [& args] (apply multi-arity-function args))")
+  (a ( = (multi-arity-macro) [0 1 2 3]))
+  (a ( = (multi-arity-macro \a) [\a 1 2 3]))
+  (m "https://stackoverflow.com/questions/25566146/multiple-arity-in-defmacro-of-clojure/25569059"))
+
+(a (defmacro call [this method & xs] `(let [this# ~this] ((~method this#) this# ~@xs))))
 
 (qam
   (q "Why did the exception occur?")
   (a (def person-class {:get-full-name (fn [this] (str (:first-name this) " " (:last-name this)))}))
   (a (def john (merge person-class  {:first-name "John" :last-name "Smith"})))
-  (a (defmacro call [this method & xs] `(let [this# ~this] ((~method this#) this# ~@xs))))
-  ;(a (= (call john :get-full-name) "John Smith"))
-  (a false "todo")
+  (a "(defmacro call [this method & xs] `(let [this# ~this] ((~method this#) this# ~@xs)))")
+  (a (= (call john :get-full-name) "John Smith"))
   (m "https://stackoverflow.com/questions/5024211/clojure-adding-functions-to-defrecord-without-defining-a-new-protocol"))
+
+(qam
+  (q "Discuss the macro and clojure.test.")
+  (a "dfine macro outside clojure.test/deftest"))
