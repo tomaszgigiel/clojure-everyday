534078303d749bf5b8a261394e3391817ab15e99
Tomasz Gigiel <tomasz.gigiel@gmail.com>
2020-02-07 11:50:02 +0100

    a star

--- batch/msdos/test-spec.bat
+++ batch/msdos/test-spec.bat
@@ -2,7 +2,8 @@ md "%HOMEPATH%\_delete_content\"
 pushd %~dp0\..\..
 REM call lein test :only pl.tomaszgigiel.quizzes.packs.joc.joc-07-01-test
 REM call lein test :only pl.tomaszgigiel.quizzes.packs.joc.joc-07-02-test
-call lein test :only pl.tomaszgigiel.quizzes.packs.joc.joc-07-03-test
+REM call lein test :only pl.tomaszgigiel.quizzes.packs.joc.joc-07-03-test
+call lein test :only pl.tomaszgigiel.quizzes.packs.joc.joc-07-04-test
 REM call lein test :only pl.tomaszgigiel.quizzes.parser-test
 REM call lein test :only pl.tomaszgigiel.quizzes.packs.test-test
 REM call lein test :only pl.tomaszgigiel.quizzes.packs.clojure-exponentiation-test
