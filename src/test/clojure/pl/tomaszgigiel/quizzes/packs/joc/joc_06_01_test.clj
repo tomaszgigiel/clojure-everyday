0eaa2f2a77ffe707785e17bcf65f16f8d1f3e8ba
Tomasz Gigiel <tomasz.gigiel@gmail.com>
2020-02-07 16:07:56 +0100

    being lazy

---
+++ src/test/clojure/pl/tomaszgigiel/quizzes/packs/joc/joc_06_01_test.clj
@@ -0,0 +1,8 @@
+(ns pl.tomaszgigiel.quizzes.packs.joc.joc-06-01-test
+  (:require [clojure.test :as tst])
+  (:require [pl.tomaszgigiel.quizzes.test-config :as test-config])
+  (:require [pl.tomaszgigiel.quizzes.quiz :refer [qam q a at m]])
+  (:require [pl.tomaszgigiel.utils.misc :as misc]))
+
+(tst/use-fixtures :once test-config/once-fixture)
+(tst/use-fixtures :each test-config/each-fixture)
